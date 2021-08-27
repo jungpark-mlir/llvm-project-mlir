f533b76e9791f31e1af8ec0002d85191ed7052bc
jungpark-mlir <jungwook.park@amd.com>
2021-08-27 17:41:30 +0100

    gpu to mixr

---
+++ mlir/include/mlir/Conversion/GPUToMIGraphX/GPUToMIGraphX.h
@@ -0,0 +1,36 @@
+//===-- GPUToMIGraphX fused func to code obj pass declarations ----------*- C++
+//-*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file declares the passes for the MIGraphX Dialect in MLIR.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef MLIR_CONVERSION_GPUTOMIGRAPHX_GPUTOMIGRAPHX_H
+#define MLIR_CONVERSION_GPUTOMIGRAPHX_GPUTOMIGRAPHX_H
+
+#include "mlir/Pass/Pass.h"
+
+namespace mlir {
+namespace migraphx {
+
+std::unique_ptr<Pass> createGPUToMIGraphX();
+
+/// Populates passes to convert from GPU to MIGraphX. 
+/// The pass lowers a func only contains a gpu launch and result allocation
+/// with constant numbers representing dimension into a code_obj operation
+void addGPUToMIGraphXPasses(OpPassManager &pm);
+
+/// Populates conversion passes from TOSA dialect to MIOpen dialect.
+void populateGPUToMIGraphXConversionPatterns(MLIRContext *context,
+                                            OwningRewritePatternList *patterns);
+
+} // namespace tosa
+} // namespace mlir
+
+#endif // MLIR_CONVERSION_GPUTOMIGRAPHX_GPUTOMIGRAPHX_H
---
+++ mlir/lib/Conversion/GPUToMIGraphX/GPUToMIGraphX.cpp
@@ -0,0 +1,53 @@
+//===- GPUToMIGraphX.cpp - Lowering GPU to MIGraphX Dialect -----------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// These rewriters lower from the GPU to the MIGraphX dialect.
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/Conversion/GPUToMIGraphX/GPUToMIGraphX.h"
+#include "mlir/Dialect/StandardOps/IR/Ops.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/Transforms/DialectConversion.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+
+using namespace mlir;
+
+namespace {
+class FuncToCOBJPattern : public OpRewritePattern<FuncOp> {
+public:
+  using OpRewritePattern<FuncOp>::OpRewritePattern;
+
+  LogicalResult
+  matchAndRewrite(FuncOp op,
+                  PatternRewriter &rewriter) const override {
+    auto loc = op->getLoc();
+    auto results = op->getResults();
+    auto resultType = results[0].getType().template cast<ShapedType>();
+    auto shape = resultType.getShape();
+    SmallVector<IntegerAttr, 5> shapeAttr;
+    for(auto dim: shape){
+      shapeAttr.push_back(rewriter.getI32IntegerAttr(dim));
+    }
+
+    ValueRange args({});
+    auto cop = rewriter.create<mlir::migraphx::CodeObjOp>(loc, resultType, args);
+    //cop->setAttr("kernel",
+//                 );
+    rewriter.replaceOp(op, cop->getResults());
+
+    return success();
+  }
+};
+
+} // namespace
+
+void mlir::migraphx::populateFuncToCOBJPatterns(
+    MLIRContext *context, OwningRewritePatternList *patterns) {
+  patterns->insert<FuncToCOBJPattern>(context);
+}
