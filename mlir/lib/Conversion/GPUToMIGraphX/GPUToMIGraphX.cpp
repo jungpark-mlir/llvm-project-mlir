0eca4345e9e618aceaacbe887f5b590af9e642ce
jungpark-mlir <jungwook.park@amd.com>
2021-09-03 13:56:06 +0100

    Update GPUToMIGraphX.cpp

--- mlir/lib/Conversion/GPUToMIGraphX/GPUToMIGraphX.cpp
+++ mlir/lib/Conversion/GPUToMIGraphX/GPUToMIGraphX.cpp
@@ -56,7 +56,7 @@ public:
       auto gridSize = Lop.getGridSizeOperandValues();
       auto blockSize = Lop.getBlockSizeOperandValues();
 
-      globalSizeAttr.push_back((gridSize.z)->getAttrOfType<IntegerAttr>("value"));
+      globalSizeAttr.push_back((gridSize.z->getOperation())->getAttrOfType<IntegerAttr>("value"));
       globalSizeAttr.push_back((gridSize.y)->getAttrOfType<IntegerAttr>("value"));
       globalSizeAttr.push_back((gridSize.x)->getAttrOfType<IntegerAttr>("value"));
       localSizeAttr.push_back((blockSize.z)->getAttrOfType<IntegerAttr>("value"));
