696fd66de9fd94b7970a378d5222927fe9a3c51d
jungpark-mlir <jungwook.park@amd.com>
2021-09-30 11:59:29 +0100

    Update GPUToMIGraphX.cpp

--- mlir/lib/Conversion/GPUToMIGraphX/GPUToMIGraphX.cpp
+++ mlir/lib/Conversion/GPUToMIGraphX/GPUToMIGraphX.cpp
@@ -91,7 +91,7 @@ class FuncToCOBJPattern : public OpConversionPattern<CallOp> {
         auto offsetOp = rewriter.create<mlir::migraphx::ConstantOp>(loc, rewriter.getI64Type(), noArgs);
         
         //offsetOp->setAttr("value", rewriter.getArrayAttr(ArrayRef<Attribute>(rewriter.getI64IntegerAttr(0))));
-        offsetOp->setAttr("value", rewriter.getI64ArrayAttr({0,0}));
+        offsetOp->setAttr("value", DenseElementsAttr::get(rewriter.getI64Type(), 0)));
         cobjArgs.push_back(offsetOp);
 
         // shape
