0519faaf495257e47ff764bea6dd6be109d55be7
jungpark-mlir <jungwook.park@amd.com>
2022-08-19 05:57:27 +0100

    ver0.1

--- mlir/include/mlir-c/Dialect/MIGraphX.h
+++ mlir/include/mlir-c/Dialect/MIGraphX.h
@@ -18,7 +18,59 @@
 extern "C" {
 #endif
 
-MLIR_DECLARE_CAPI_DIALECT_REGISTRATION(MIGraphX, migraphx);
+enum MlirMIOpenConvLayout { NCHW, NHWC };
+enum MlirMIOpenOpDataType { FP32, FP16, BF16, INT8, INT32 };
+enum MlirMIOpenConvDirection { FWD, BWD, WT };
+enum MlirMIOpenTunableOpType { conv2d, gemm };
+
+// Problem config struct, using existing struct from the MIOpen tuning DB
+struct conv2dConfig {
+  MlirMIOpenConvLayout layout;
+  MlirMIOpenOpDataType data_type;
+  MlirMIOpenConvDirection direction;
+  // spatial_dim
+  uint64_t in_channels;
+  uint64_t in_h;
+  uint64_t in_w;
+  // in_d
+  uint64_t fil_h;
+  uint64_t fil_w;
+  // fil_d
+  uint64_t out_channels;
+  uint64_t batchsize;
+  uint64_t pad_h;
+  uint64_t pad_w;
+  // pad_d
+  uint64_t conv_stride_h;
+  uint64_t conv_stride_w;
+  // conv_stride_d
+  uint64_t dilation_h;
+  uint64_t dilation_w;
+  // dilation_d
+  // bias
+  uint64_t group_count;
+};
+
+struct gemmConfig {
+  uint64_t M;
+  uint64_t K;
+  uint64_t N;
+};
+
+union MlirMIOpenTunableOpConfig {
+  MlirMIOpenTunableOpType opType;
+  struct conv2dConfig convCfg;
+  struct gemmConfig gemmCfg;
+};
+
+static int32_t getNumTuningParams(union MlirMIOpenTunableOpConfig op){
+  if (op.opType == conv2d)
+    return 8;
+  else
+    return 0; // unsupported.
+}
+
+    MLIR_DECLARE_CAPI_DIALECT_REGISTRATION(MIGraphX, migraphx);
 
 // Phase 0 functions : Assuming the given module contains only one function
 
@@ -33,6 +85,18 @@ MLIR_CAPI_EXPORTED void mlirGetKernelAttrs(MlirModule module, uint32_t *attrs);
 // Returns the size of compiled binary if called with null ptr
 // and return the compiled binary when buffer is provided
 MLIR_CAPI_EXPORTED bool mlirGetBinary(MlirModule module, int *size, char *bin);
+
+// Find tunable op and update perf_config attribute
+MLIR_CAPI_EXPORTED bool mlirSetTuningParams(MlirModule module,
+                                            MlirMIOpenTunableOpConfig problem,
+                                            void *perfConfig, bool bPerfStr);
+
+// Find tunable op and get tuning information
+// returns the number of tuning parameters
+// returns the range of each parameters in tuningRanges
+MLIR_CAPI_EXPORTED unsigned
+mlirGetTuningInfo(MlirModule module, MlirMIOpenTunableOpConfig *problem,
+                  unsigned *tuningRanges);
 
 // pipelines
 
--- mlir/lib/CAPI/Dialect/MIGraphX.cpp
+++ mlir/lib/CAPI/Dialect/MIGraphX.cpp
@@ -99,6 +99,210 @@ MLIR_CAPI_EXPORTED bool mlirGetBinary(MlirModule module, int *size, char *bin) {
   return success;
 }
 
+//
+std::string stringfyConfig(int32_t numParams, uint32_t *perfConfig) {
+  std::string result;
+  int i = 0;
+  for (; i < numParams - 1; i++){
+    result.append(perfConfig[i]);
+    result.append(',');
+  }
+  result.append(perfConfig[i]);
+  return result;
+}
+
+void getRealConv2dPerfConfig(MlirMIOpenOpDataType dType, int32_t nParam, uint32_t *pConfig) {
+    // bool GemmBThreadCopyMoreGemmKPack
+    // bool GemmAThreadCopyMoreGemmK
+    // <4, 256> GemmMPerBlock
+    constexpr std::vector<uint32_t> p2 = {4, 8, 16, 32, 64, 128, 256};
+    // <16, 256> GemmNPerBlock
+    constexpr std::vector<uint32_t> p3 = {16, 32, 64, 128, 256};
+    // <4, 128> GemmMPerWave
+    constexpr std::vector<uint32_t> p4 = {4, 8, 16, 32, 64, 128};
+    // <16, 128> GemmNPerWave
+    constexpr std::vector<uint32_t> p5 = {16, 32, 64, 128};
+    // <4, 8> GemmKPACKSize
+    constexpr std::vector<uint32_t> p6 = {4, 8};
+    // INT8<8, 32> or <1, 8> Gem1mKPerBlock
+    constexpr std::vector<uint32_t> p7a = {8, 16, 32};
+    constexpr std::vector<uint32_t> p7b = {1, 2, 4, 8};
+
+    // Replace the index with the real value.
+    pConfig[2] = p2[pConfig[2]];
+    pConfig[3] = p3[pConfig[3]];
+    pConfig[4] = p4[pConfig[4]];
+    pConfig[5] = p5[pConfig[5]];
+    pConfig[6] = p6[pConfig[6]];
+    if (dType == INT32)
+      pConfig[7] = p7a[pConfig[7]];
+    else
+      pConfig[7] = p7b[pConfig[7]];
+}
+
+// Traverse tunable ops and update perf_config attribute
+MLIR_CAPI_EXPORTED bool mlirSetTuningParams(MlirModule module,
+                                            MlirMIOpenTunableOpConfig problem,
+                                            void *perfConfig, bool bPerfStr) {
+  if (!perfConfig)
+    return false; // fail as nullptr passed.
+  int32_t numParams;
+  bool bFound = false;
+  auto mod = unwrap(module);
+  mod.walk([&](mlir::miopen::Conv2DOp convOp) {
+    if (bFound)
+      return false; // fail because the module contains multiple tunable ops.
+    bFound = true;
+    if (problem.opType != conv2d)
+      return false; // invalid op requested.
+    ImplicitLocOpBuilder b(convOp->getLoc(), convOp->getContext());
+    numParams = getNumTuningParams(problem);
+    if (bPerfStr) { // set string attr
+      StringAttr attr = b.getStringAttr(static_cast<std::string*>(perfConfig));
+      convOp->setAttr("perf_config", attr);
+    }
+    else {
+      // get real values first.
+      getRealConv2dPerfConfig(numParams, perfConfig, problem.convCfg.data_type);
+      // stringfy and set attribute
+      std::string perfString = stringfyConfig(numParams, perfConfig);
+      StringAttr attr = b.getStringAttr(perfString);
+      convOp->setAttr("perf_config", attr);
+    }
+  });
+
+  mod.walk([&](mlir::miopen::GemmOp gemmOp) {
+    return false; // fail, unsupported yet
+  });
+
+  return true; // succeed.
+}
+
+// returns information so user can create a tuning space, OwnedRanges
+MLIR_CAPI_EXPORTED int32_t mlirGetTuningInfo(MlirModule module,
+                                             MlirMIOpenTunableOpConfig *problem,
+                                             uint32_t *tuningRanges) {
+  if (!problem || !tuningRanges)
+    return 0; // fail as nullptr passes for the tuning.
+
+  int32_t numParams;
+  bool bFound = false;
+  auto mod = unwrap(module);
+
+  // miopen.conv2d
+  mod.walk([&](mlir::miopen::Conv2DOp convOp) {
+    if (bFound)
+      return 0; // fail because the module contains multiple tunable ops.
+    bFound = true;
+    problem.opType = conv2d;
+    problem.convCfg.direction = FWD;
+
+    // Extract convolution config.
+    StringRef inLayout =
+        convOp->getAttrOfType<StringAttr>("input_layout").getValue();
+    StringRef filterLayout =
+        convOp->getAttrOfType<StringAttr>("filter_layout").getValue();
+    StringRef outputLayout =
+        convOp->getAttrOfType<StringAttr>("output_layout").getValue();
+
+    // Only expect these two combinations for now.
+    StringRef iNCHW = "nchwg";
+    StringRef fNCHW = "kcyxg";
+    StringRef oNCHW = "nkhwg";
+    StringRef iNHWC = "nhwcg";
+    StringRef fNHWC = "kyxcg";
+    StringRef oNHWC = "nhwkg";
+
+    if (inLayout.compare(iNCHW) == 0) {
+      if (filterLayout.compare(fNCHW) != 0 || outputLayout.compare(oNCHW) != 0)
+        return 0; // unexpected layout, failing.
+      problem.convCfg.layout = NCHW;
+    } else if (inLayout.compare(iNHWC) == 0) {
+      if (filterLayout.compare(fNHWC) != 0 || outputLayout.compare(oNHWC) != 0)
+        return 0; // unexpected layout, failing.
+      problem.convCfg.layout = NHWC;
+    } else {
+      return 0; // unexpected layout, failing.
+    }
+
+    auto input = convOp->getOperand(0);
+    auto weight = convOp->getOperand(1);
+    ShapedType inTy = input.getType().cast<ShapedType>();
+    auto inShape = inTy.getShape();
+    problem.convCfg.batchsize = inShape[0];
+
+    auto elemTy = inTy.getElementType();
+    if (elemTy.isF32())
+      problem.convCfg.data_type = FP32;
+    else if (elemTy.isF16())
+      problem.convCfg.data_type = FP16;
+    else if (elemTy.isBF16())
+      problem.convCfg.data_type = BF16;
+    else if (elemTy.isInteger(32))
+      problem.convCfg.data_type = INT32;
+    else if (elemTy.isInteger(8))
+      problem.convCfg.data_type = INT8;
+    else
+      return 0; // unsupported type, failing.
+
+    ShapedType wtTy = weight.getType().cast<ShapedType>();
+    auto wtShape = wtTy.getShape();
+    problem.convCfg.out_channels = wtShape[0];
+
+    if (problem.convCfg.layout == NCHW) {
+      problem.convCfg.in_channels = inShape[1];
+      problem.convCfg.in_h = inShape[2];
+      problem.convCfg.in_w = inShape[3];
+      problem.convCfg.f_h = wtShape[2];
+      problem.convCfg.f_w = wtShape[3];
+    } else { // NHWC
+      problem.convCfg.in_channels = inShape[3];
+      problem.convCfg.in_h = inShape[2];
+      problem.convCfg.in_w = inShape[3];
+      problem.convCfg.f_h = wtShape[2];
+      problem.convCfg.f_w = wtShape[3];
+    }
+
+    auto pad = convOp.pad();
+    auto stride = convOp.stride();
+    auto dilation = convOp.dilation();
+
+    // pad top and left instead.
+    problem.convCfg.pad_h = pad[0].dyn_cast<IntegerAttr>().getInt();
+    problem.convCfg.pad_w = pad[2].dyn_cast<IntegerAttr>().getInt();
+    problem.convCfg.conv_stride_h = stride[0].dyn_cast<IntegerAttr>().getInt();
+    problem.convCfg.conv_stride_w = stride[1].dyn_cast<IntegerAttr>().getInt();
+    problem.convCfg.dilation_h = dilation[0].dyn_cast<IntegerAttr>().getInt();
+    problem.convCfg.dilation_w = dilation[1].dyn_cast<IntegerAttr>().getInt();
+
+    problem.convCfg.group_count = inShape[4];
+
+    numParams = getNumTuningParams(problem);
+    // bool GemmBThreadCopyMoreGemmKPack
+    // bool GemmAThreadCopyMoreGemmK
+    // <4, 256> GemmMPerBlock
+    // <16, 256> GemmNPerBlock
+    // <4, 128> GemmMPerWave
+    // <16, 128> GemmNPerWave
+    // <4, 8> GemmKPACKSize
+    // INT8<8, 32> or <1, 8> Gem1mKPerBlock
+    std::vector<uint32_t> ranges = {2, 2, 7, 5, 6, 4, 2, 4};
+    if (problem.convCfg.data_type == INT8)
+      ranges[7] = 3;
+    tuningRanges = ranges.data();
+  });
+
+  // miopen.gemm
+  mod.walk([&](mlir::miopen::GemmOp gemmOp) {
+    if (bFound)
+      return 0; // fail because the module contains multiple tunable ops.
+    bFound = true;
+    numParams = 0; // Not supported yet
+  });
+
+  return numParams;
+}
+
 // pipelines
 
 MLIR_CAPI_EXPORTED
